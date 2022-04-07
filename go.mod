7158de5ea878fe408b3e2821ffd22fe50c3d9e50
Teodoro Alcántara Aquino <teodoro641@hotmail.com>
2022-04-07 06:35:52 -0400

    Changed project files structure

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module mirai-api
 go 1.17
 
 require (
-	github.com/marti700/mirai v0.0.0-20220316122428-18259ce23646
+	github.com/marti700/mirai v0.0.0-20220406121543-07fb44d3d390
 	github.com/marti700/veritas v0.0.0-20220316114542-b8896a01b8a4
 )
--- main.go
+++ main.go
@@ -1,74 +1,27 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"mime/multipart"
-	"mirai-api/instruction"
 	"net/http"
-	"strconv"
-	"strings"
 
-	"github.com/marti700/veritas/linearalgebra"
+	"mirai-api/parser/data"
+	"mirai-api/parser/instruction"
+
 )
 
-// Reads data from a csv file and returns the read data as a Matrix
-// this functiona assumes the data in the csv are numbers in the float64 range
-func ReadDataFromcsv(f multipart.File) linearalgebra.Matrix {
 
-	scanner := bufio.NewScanner(f)
-	var matrixData string
-
-	// read file first line to get the matrix column number
-	// this are the heading numbers of the csv files
-	// this line can be discarted since it does not hold useful data
-	scanner.Scan()
-	fstLine := scanner.Text()
-	cols_num := len(strings.Split(fstLine, ","))
-
-	// loop through the rest of the file
-	fileLines := 0
-	for scanner.Scan() {
-		// extra coma so that the last number of this line don't get mixed with the first number of the next when slitting later
-		matrixData += scanner.Text()+","
-		fileLines++
-	}
-
-	dataSplit := strings.Split(matrixData, ",")
-
-	matrix := make([][]float64, fileLines)
-	row := 0
-	col := 0
-	nextBreak := cols_num
-
-	data := make([]float64, cols_num)
-	for i, e := range dataSplit {
-		//before processing the matrix next row
-		if i == nextBreak {
-			matrix[row] = data
-			data = make([]float64, cols_num)
-			nextBreak += cols_num
-			row++
-			col = 0
-		}
-		data[col], _ = strconv.ParseFloat(e, 64)
-		col++
-	}
-	fmt.Println(linearalgebra.NewMatrix(matrix))
-	return linearalgebra.NewMatrix(matrix)
-}
 
 func HandleUpload(w http.ResponseWriter, r *http.Request){
 	r.ParseMultipartForm(200)
-	instructions, handler, _ := r.FormFile("json")
-	data, handler, _ := r.FormFile("train")
-	target, handler, _ := r.FormFile("target")
-	defer instructions.Close()
-	defer data.Close()
-	defer target.Close()
-	trainData := ReadDataFromcsv(data)
-	targetData := ReadDataFromcsv(target)
-	instruction.ParseInstruction(instructions, trainData, targetData)
+	instructionsFile, _, _ := r.FormFile("json")
+	dataFile, _, _ := r.FormFile("train")
+	targetFile, handler, _ := r.FormFile("target")
+	defer instructionsFile.Close()
+	defer dataFile.Close()
+	defer targetFile.Close()
+	trainData := data.ReadDataFromcsv(dataFile)
+	targetData := data.ReadDataFromcsv(targetFile)
+	instruction.ParseInstruction(instructionsFile, trainData, targetData)
 	fmt.Println(handler.Filename)
 	fmt.Println(handler.Size)
 	fmt.Println("ja")
