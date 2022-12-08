e9d2ab66c73c37716542067c9e4336550e6ee68c
Teodoro Alcantara Aquino <teodoro641@hotmail.com>
2022-12-08 17:30:58 -0400

    Added Tree model initializers

--- parser/instruction/initializer.go
+++ parser/instruction/initializer.go
@@ -1,37 +1,72 @@
 package instruction
 
 import (
+	"github.com/marti700/mirai/metrics"
 	model "github.com/marti700/mirai/models"
 	"github.com/marti700/mirai/models/linearmodels"
+	"github.com/marti700/mirai/models/treemodels"
 	"github.com/marti700/mirai/options"
 )
 
-func initalizeLinRegModel(trainIns []LinearRegInstructions) []model.Model {
+// Recieves a LinearRegressionInstructions slice to initialize linear regression models
+func initalizeLinRegModel(trainIns []LinearRegInstructions) []map[string]model.Model {
+	mod := make([]map[string]model.Model, len(trainIns))
+	// modelChanel := make(chan model.Model)
 
-	mod := make([]model.Model, len(trainIns))
-	modelChanel := make(chan model.Model)
-
-	for _, ins := range trainIns {
-		go func(i LinearRegInstructions, ch chan model.Model) {
-			lr := linearmodels.LinearRegression{}
-			if i.Estimators.OLS {
-				lr.Opts = options.LROptions{
-					Estimator:      options.OLSOptions{},
-					Regularization: i.Regularization,
-				}
-			} else {
-				lr.Opts = options.LROptions{
-					Estimator:      i.Estimators.GD,
-					Regularization: i.Regularization,
-				}
+	for i, ins := range trainIns {
+		lr := linearmodels.LinearRegression{}
+		if ins.Estimators.OLS {
+			lr.Opts = options.LROptions{
+				Estimator:      options.OLSOptions{},
+				Regularization: ins.Regularization,
 			}
-			ch <- &lr
-		}(ins, modelChanel)
+		} else {
+			lr.Opts = options.LROptions{
+				Estimator:      ins.Estimators.GD,
+				Regularization: ins.Regularization,
+			}
+		}
+		m := make(map[string]model.Model)
+		m[ins.Name] = &lr
+		mod[i] = m
 	}
 
-	for i := range mod {
-		val := <-modelChanel
-		mod[i] = val
+	return mod
+}
+
+func initializeDecisionTreeClassifierModel(trainIns []DecisiontreeClassIntruction) []map[string]model.Model {
+	mod := make([]map[string]model.Model, len(trainIns))
+
+	//Initialize models
+	for i, ins := range trainIns {
+		c_opt := options.NewDTreeClassifierOption(ins.Criterion)
+		treeModel := treemodels.NewDecicionTreeeClassifier(c_opt)
+
+		m := make(map[string]model.Model)
+		m[ins.Name] = treeModel
+		mod[i] = m
+	}
+
+	return mod
+}
+
+func initializeDecisionTreeRegressorModel(trainIns []DecisiontreeRegIntruction) []map[string]model.Model {
+	mod := make([]map[string]model.Model, len(trainIns))
+
+	//Initialize models
+	for i, ins := range trainIns {
+		var reg_opt options.DTreeRegreessorOptions
+		if ins.Criterion == "RSS" {
+			reg_opt = options.NewDTRegressorOptions(ins.MinLeafSamples, metrics.RSS)
+		} else if ins.Criterion == "MSE" {
+			reg_opt = options.NewDTRegressorOptions(ins.MinLeafSamples, metrics.MeanSquareError)
+		}
+
+		treeModel := treemodels.NewDecisionTreeRegressor(reg_opt)
+
+		m := make(map[string]model.Model)
+		m[ins.Name] = treeModel
+		mod[i] = m
 	}
 
 	return mod
--- parser/instruction/instructions.go
+++ parser/instruction/instructions.go
@@ -21,9 +21,9 @@ type Instruction interface {
 // the Instructions field are the instructions used to train the model
 
 type Instructions struct {
-	InstructionType string        `json:modelType`
-	Name            string        `json:name`
-	Models          []model.Model `json:instructions`
+	InstructionType string                   `json:modelType`
+	Name            string                   `json:name`
+	Models          []map[string]model.Model `json:instructions`
 }
 
 // Creates and returns an empty Instructions Entity
@@ -53,7 +53,7 @@ func NewInstructions() Instructions {
 //     }
 //   },
 //   {
-//     "InstructionType": "DecisionTreeClassifier",
+//     "InstructionType": "decisiontreeclassifier",
 //     "name": "second Instruction DTC",
 //     "instructions": {
 //       "name": "Instruction1",
@@ -62,7 +62,7 @@ func NewInstructions() Instructions {
 //     }
 //   },
 //   {
-//     "InstructionType": "DecisionTreeRegressor",
+//     "InstructionType": "decisiontreeregressor",
 //     "name": "third Instruction",
 //     "instructions": {
 //       "name": "Instruction3",
@@ -76,12 +76,9 @@ func NewInstructions() Instructions {
 func (i Instructions) Parse(f multipart.File) []Instructions {
 	filebytes, _ := ioutil.ReadAll(f)
 	inss := make([]Instructions, 0, 200)
-	// mod := make([]model.Model, 200)
 
 	var objmap []interface{}
 	err := json.Unmarshal(filebytes, &objmap)
-	// j := objmap[0]
-	// r := j.(map[string]interface{})
 
 	for _, ins := range objmap {
 		instruction := ins.(map[string]interface{})
@@ -89,7 +86,6 @@ func (i Instructions) Parse(f multipart.File) []Instructions {
 		if insType == "linearregression" {
 			//create model initializers package to initialize model depending on their types ej: linRegInitializer, TreeInitializer, etc
 			linRegIns := getLinearRegIns(instruction["instructions"].([]interface{}))
-			// copy(mod, initalizeLinRegModel(linRegIns))
 
 			regModels := Instructions{
 				InstructionType: insType,
@@ -98,23 +94,44 @@ func (i Instructions) Parse(f multipart.File) []Instructions {
 			}
 			inss = append(inss, regModels)
 		}
+
+		if insType == "decisiontreeclassifier" {
+			dtc := getDTCIns(instruction["instructions"].([]interface{}))
+
+			dtcModel := Instructions{
+				InstructionType: insType,
+				Name:            instruction["name"].(string),
+				Models:          initializeDecisionTreeClassifierModel(dtc),
+			}
+
+			inss = append(inss, dtcModel)
+		}
+		if insType == "decisiontreeregressor" {
+			dtr := getDTRegIns(instruction["instructions"].([]interface{}))
+
+			dtrModel := Instructions{
+				InstructionType: insType,
+				Name:            instruction["name"].(string),
+				Models:          initializeDecisionTreeRegressorModel(dtr),
+			}
+
+			inss = append(inss, dtrModel)
+
+		}
 	}
-	// err1 := json.Unmarshal(r, &li)
 
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	// fmt.Println(r)
 
 	return inss
 }
 
 func getLinearRegIns(ins []interface{}) []LinearRegInstructions {
 	var linRegIns []LinearRegInstructions
-	// x := h.([]interface{})
+	// marshals the ins parameter to get an array of bytes
+	// it will be used later to convert it to an LinearRegInstructions
 	b, _ := json.Marshal(ins)
-	// b := x[0].([]byte)
 	error := json.Unmarshal(b, &linRegIns)
 
 	if error != nil {
@@ -122,4 +139,28 @@ func getLinearRegIns(ins []interface{}) []LinearRegInstructions {
 	}
 
 	return linRegIns
+}
+
+func getDTCIns(ins []interface{}) []DecisiontreeClassIntruction {
+	var dtcIns []DecisiontreeClassIntruction
+	b, _ := json.Marshal(ins)
+	error := json.Unmarshal(b, &dtcIns)
+
+	if error != nil {
+		log.Fatal(error)
+	}
+
+	return dtcIns
+}
+
+func getDTRegIns(ins []interface{}) []DecisiontreeRegIntruction {
+	var dtrIns []DecisiontreeRegIntruction
+	b, _ := json.Marshal(ins)
+	error := json.Unmarshal(b, &dtrIns)
+
+	if error != nil {
+		log.Fatal(error)
+	}
+
+	return dtrIns
 }
